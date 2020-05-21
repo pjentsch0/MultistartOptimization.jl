eadb3db69c05180432d123b0e08afc017e77cb0c
Peter C. Jentsch <pjentsch@uwaterloo.ca>
2020-05-21 10:50:42 -0400

    more general NLopt interface

--- src/MultistartOptimization.jl
+++ src/MultistartOptimization.jl
@@ -90,39 +90,10 @@ Base.@kwdef struct NLoptLocalMethod
 end
 
 """
-$(SIGNATURES)
-
-A wrapper for algorithms supported by `NLopt`. Used to construct the corresponding
-optimization problem.
-
-See the NLopt documentation for the options. Defaults are changed slightly.
-"""
-function NLoptLocalMethod(algorithm::NLopt.Algorithm; options...)
-    NLoptLocalMethod(; algorithm = algorithm, options...)
-end
-
-"""
-$(SIGNATURES)
-
 Solve `minimization_problem` using `local_method`, starting from `x`. Return a
 `LocationValue`.
 """
-function local_minimization(local_method::NLoptLocalMethod,
-                            minimization_problem::MinimizationProblem, x)
-    @unpack algorithm, xtol_abs, xtol_rel, maxeval, maxtime = local_method
-    @unpack objective, lower_bounds, upper_bounds = minimization_problem
-    opt = NLopt.Opt(algorithm, length(x))
-    opt.lower_bounds = lower_bounds
-    opt.upper_bounds = upper_bounds
-    function f̃(x, grad)         # wrapper for NLopt
-        @argcheck isempty(grad) # ensure no derivatives are asked for
-        objective(x)
-    end
-    opt.min_objective = f̃
-    opt.xtol_abs = xtol_abs
-    opt.xtol_rel = xtol_rel
-    opt.maxeval = maxeval
-    opt.maxtime = maxtime
+function local_minimization(opt, x)
     optf, optx, ret = NLopt.optimize(opt, x)
     # FIXME ret is ignored, save it? is it useful?
     LocationValue(optx, optf)
